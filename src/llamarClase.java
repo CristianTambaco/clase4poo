4ae1197618dd2181bbcfaf8211cd92974192303f
POO <1234cristiant@gmail.com>
2024-10-17 08:00:05 -0500

    commit

---
+++ src/llamarClase.java
@@ -0,0 +1,14 @@
+
+//clase llama al los objetos
+public class llamarClase {
+
+
+    public static void main(String[] args) {
+
+        vehiculos auto1=new vehiculos("Toyota",2015,"rojo"); //
+        auto1.imprimir();
+
+    }
+
+
+}
---
+++ src/vehiculos.java
@@ -0,0 +1,35 @@
+public class vehiculos {
+
+    //atributo de la clase vehiculos
+    String modelo;
+    int anio;
+    String color;
+
+    //metodo constructor con 3 atributos
+    public vehiculos(String modelo, int anio, String color){
+        this.modelo = modelo;
+        this.anio = anio;
+        this.color = color;
+    }
+
+    //metodo vacío, no devuelve nada
+    public void imprimir(){
+        System.out.println("El modelo es: "+ modelo +
+                           ", el anio del vehículo es "+ anio +
+                           ", el color del vehículo es " + color);
+
+    }
+
+    //vehiculo del metodo constructor, aqui es donde se crean los objetos
+    public static void main(String[] args) {
+        vehiculos auto1=new vehiculos("Toyota", 2024, "negro"); //consumiendo de la clase
+        vehiculos auto2=new vehiculos("chevrolet", 2012, "plateado");
+        auto1.imprimir();
+        auto2.imprimir();
+
+
+    }
+}
+
+
+
